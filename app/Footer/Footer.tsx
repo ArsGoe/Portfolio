3a715565cbb32693f8ba4e27c98f5eaab0fd9bcc
Roméo <romeo.jacquemine@gmail.com>
2024-12-23 20:05:47 +0100

    voilà

--- app/Footer/Footer.tsx
+++ app/Footer/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <footer className="bg-footer text-white p-4">
             <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
                 <div className="mb-4 md:mb-0">
-                    <h2 className="text-xl font-bold">Follow Me</h2>
+                    <h2 className="text-xl font-bold">Suivez mon actualité</h2>
                     <div className="mt-2 flex space-x-4">
                         <a href="https://fr.linkedin.com/in/roméo-jacquemin" target="_blank" rel="noopener noreferrer"
                            className="text-text hover:underline">
@@ -21,7 +21,7 @@ const Footer = () => {
                     </div>
                 </div>
                 <div>
-                    <h2 className="text-xl font-bold">Contact</h2>
+                    <h2 className="text-xl font-bold">Mon Mail</h2>
                     <p className="mt-2">romeo.jacquemine@gmail.com</p>
                 </div>
             </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {Col, Progress, Row} from "antd";
 
 const HomePage = () => {
 
@@ -8,12 +9,6 @@ const HomePage = () => {
             endroit: 'IUT de Lens',
             duree: '2022 - Aujourd’hui',
             plus: 'Formation informatique avec spécialisation dans les applications web.'
-        },
-        {
-            nom: 'Baccalauréat',
-            endroit: 'Lycée Arthur Rimbaud, Sin-le-Noble',
-            duree: 'Juin 2022',
-            plus: 'Baccalauréat avec spécialité Mathématiques, Sciences physiques/Chimie avec option maths experts.'
         },
         {
             nom: 'Stage en intelligence artificielle et robotique',
@@ -32,27 +27,34 @@ const HomePage = () => {
             endroit: 'IUT de Lens',
             duree: '2023 - 2024',
             plus: 'J\'ai, penddant une année, été le président d\'une association étudiante à l\'IUT de Lens.'
+        },
+        {
+            nom: 'Baccalauréat',
+            endroit: 'Lycée Arthur Rimbaud, Sin-le-Noble',
+            duree: 'Juin 2022',
+            plus: 'Baccalauréat avec spécialité Mathématiques, Sciences physiques/Chimie avec option maths experts.'
         }
     ];
 
     const skills = [
-        {name: 'Angular', percentage: '70%'},
-        {name: 'Canva', percentage: '60%'},
-        {name: 'Git', percentage: '85%'},
-        {name: 'HTML/CSS', percentage: '80%'},
-        {name: 'Java', percentage: '75%'},
-        {name: 'JavaFX / Scenebuilder', percentage: '65%'},
-        {name: 'Laravel', percentage: '70%'},
-        {name: 'MongoDB', percentage: '75%'},
-        {name: 'Neo4J', percentage: '70%'},
-        {name: 'NoSQL', percentage: '65%'},
-        {name: 'PHP', percentage: '70%'},
-        {name: 'Python', percentage: '80%'},
-        {name: 'React', percentage: '80%'},
-        {name: 'SQL', percentage: '65%'},
-        {name: 'Symphony', percentage: '70%'},
-        {name: 'TypeScript', percentage: '75%'}
+        { name: 'Git', percentage: 85 },
+        { name: 'Python', percentage: 80 },
+        { name: 'React', percentage: 80 },
+        { name: 'HTML/CSS', percentage: 80 },
+        { name: 'Java', percentage: 75 },
+        { name: 'MongoDB', percentage: 75 },
+        { name: 'TypeScript', percentage: 75 },
+        { name: 'Angular', percentage: 70 },
+        { name: 'Laravel', percentage: 70 },
+        { name: 'Neo4J', percentage: 70 },
+        { name: 'PHP', percentage: 70 },
+        { name: 'Symphony', percentage: 70 },
+        { name: 'Canva', percentage: 60 },
+        { name: 'JavaFX / Scenebuilder', percentage: 65 },
+        { name: 'NoSQL', percentage: 65 },
+        { name: 'SQL', percentage: 65 }
     ];
+
 
     const projects = [
         {
@@ -80,57 +82,86 @@ const HomePage = () => {
 
     return (
         <div className="min-h-screen bg-background p-4">
-            <header className="text-center my-8">
-                <h1 className="text-4xl font-bold">Roméo JACQUEMIN</h1>
-            </header>
-            <section id="about" className="bg-background text-white p-8">
-                <div className="container mx-auto flex flex-col md:flex-row items-center bg-footer">
-                    <div className="bg-footer">
-                        <h2 className="text-3xl font-bold mb-4 text-text">À PROPOS DE MOI</h2>
-                        <p>Roméo Jacquemin, 20 ans, étudiant à l`IUT de Lens, en BUT Informatique et en intelligence
-                            artificielle.
-                            Pendant mes trois années à l`IUT de Lens, j`ai pu apprendre à programmer dans différents
-                            languages pour différentes tâches,
-                            du web, de la base de données, de la POO, etc.
+            <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+                <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+                    Roméo Jacquemin
+                </h1>
+                <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+                    Il s'agit de mon portfolio étudiant comptant mes projets réalisés pendant ma formation de BUT
+                    Informatique à l'IUT de Lens
+                </p>
+            </div>
+            <h2 className="text-3xl font-bold mb-4 text-text">À PROPOS DE MOI</h2>
+            <p>Roméo Jacquemin, 20 ans, étudiant à l`IUT de Lens, en BUT Informatique et en intelligence
+                artificielle.
+                Pendant mes trois années à l`IUT de Lens, j`ai pu apprendre à programmer dans différents
+                languages pour différentes tâches,
+                du web, de la base de données, de la POO, etc.
+            </p>
+            <br/>
+            <h2 className="text-3xl font-bold mb-8 text-text">FORMATIONS / EXPERIENCES</h2>
+            <ol className="relative border-s border-gray-200 dark:border-gray-700">
+                {experiences.map((exp, index) => (
+                    <li key={index} className="mb-10 ms-4">
+                        <div
+                            className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                        <time
+                            className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                            {exp.duree}
+                        </time>
+                        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                            {exp.nom} - {exp.endroit}
+                        </h3>
+                        <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+                            {exp.plus}
                         </p>
-                    </div>
-                </div>
-                <br/>
-                <div className="container mx-auto bg-footer">
-                    <h2 className="text-3xl font-bold mb-8 text-text">FORMATIONS / EXPERIENCES</h2>
-                    {experiences.map((exp, index) => (
-                        <div key={index} className="mb-8 bg-box">
-                            <h3 className="text-2xl font-semibold">{exp.nom}</h3>
-                            <p className="text-xl">{exp.endroit} - {exp.duree}</p>
-                            <p>{exp.plus}</p>
-                        </div>
+                    </li>
+                ))}
+            </ol>
+            <h2 className="text-3xl font-bold mb-8 text-text">MES COMPETENCES</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                <Row gutter={[16, 16]}>
+                    {skills.map((skill, index) => (
+                        <Col span={12} key={index}>
+                            <h4>{skill.name}</h4>
+                            <Progress percent={skill.percentage} size="small"/>
+                        </Col>
                     ))}
-                </div>
-            </section>
-            <section id="skills" className="text-white p-8">
-                <div className="container mx-auto bg-footer">
-                    <h2 className="text-3xl font-bold mb-8 text-text">MES COMPETENCES</h2>
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 bg-footer">
-                        {skills.map((skill, index) => (
-                            <div key={index} className="bg-box p-4 rounded shadow-md">
-                                <h3 className="text-xl font-bold">{skill.name}</h3>
-                                <p>{skill.percentage}</p>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </section>
-            <section id="skills" className="text-white p-8">
-                <div className="container mx-auto bg-footer">
-                    <h2 className="text-3xl font-bold mb-8 text-text">MES PROJETS</h2>
+                </Row>
+            </div>
+            <br/>
+            <br/>
+
+
+
+            <h2 className="text-3xl font-bold mb-8 text-text">MES PROJETS</h2>
+            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" className="px-6 py-3">
+                        Exemples de projets
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
                     {projects.map((exp, index) => (
-                        <div key={index} className="mb-8 bg-box">
-                            <h3 className="text-2xl font-semibold">{exp.name}</h3>
-                            <p className="text-xl">{exp.description}</p>
-                        </div>
+                        <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <th scope="row"
+                                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                <h3 className="text-2xl font-semibold">{exp.name}</h3>
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                <p className="text-xl">{exp.description}</p>
+                            </th>
+                        </tr>
                     ))}
-                </div>
-            </section>
+                </tbody>
+            </table>
+
+
         </div>
     );
 };
